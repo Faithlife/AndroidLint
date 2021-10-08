95e019ac4075d91e821d0455155b847648b88987
Justin Brooks <justin.brooks@faithlife.com>
2021-10-08 15:13:22 -0500

    Add a vendor

--- checks/src/main/kotlin/com/faithlife/lint/IssueRegistry.kt
+++ checks/src/main/kotlin/com/faithlife/lint/IssueRegistry.kt
@@ -1,9 +1,17 @@
 package com.faithlife.lint
 
+import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.client.api.IssueRegistry as ApiIssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
 
 class IssueRegistry : ApiIssueRegistry() {
+    override val vendor = Vendor(
+        vendorName = "Faithlife",
+        identifier = "android-lint",
+        feedbackUrl = "https://github.com/Faithlife/AndroidLint/issues",
+        contact = "mobile@faithlife.com"
+    )
+
     override val issues = listOf(
         ObsoleteDateInstantiationDetector.ISSUE,
         SingleApostropheDetector.ISSUE,
