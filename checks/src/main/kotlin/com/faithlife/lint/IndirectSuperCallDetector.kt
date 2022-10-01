6e4621d631cb2303dcd15135a600bf70ffbbab39
Justin Brooks <justin@jzbrooks.com>
2022-10-01 14:09:52 -0500

    Pass the tests

--- checks/src/main/kotlin/com/faithlife/lint/IndirectSuperCallDetector.kt
+++ checks/src/main/kotlin/com/faithlife/lint/IndirectSuperCallDetector.kt
@@ -2,16 +2,21 @@ package com.faithlife.lint
 
 import com.android.tools.lint.client.api.UElementHandler
 import com.android.tools.lint.detector.api.*
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiClassType
 import org.jetbrains.uast.UCallExpression
 import org.jetbrains.uast.getContainingUClass
+import org.jetbrains.uast.getContainingUMethod
 
 class IndirectSuperCallDetector : Detector(), SourceCodeScanner {
     override fun getApplicableUastTypes() = listOf(UCallExpression::class.java)
 
     override fun createUastHandler(context: JavaContext) = object : UElementHandler() {
         override fun visitCallExpression(node: UCallExpression) {
-            val method = node.resolve()
-            if (node.receiverType == node.getContainingUClass()?.superTypes?.firstOrNull()) {
+            val superTypes : Set<PsiClassType> = node.getContainingUClass()?.superTypes?.toHashSet() ?: emptySet()
+            val containingMethod = node.getContainingUMethod()!!
+
+            if (containingMethod.name != node.methodName && node.receiverType in superTypes) {
                 context.report(ISSUE, context.getCallLocation(node, includeArguments = false, includeReceiver = true), "")
             }
         }
--- checks/src/test/kotlin/com/faithlife/lint/IndirectSuperCallDetectorTest.kt
+++ checks/src/test/kotlin/com/faithlife/lint/IndirectSuperCallDetectorTest.kt
@@ -1,13 +1,38 @@
 package com.faithlife.lint
 
 import com.android.tools.lint.checks.infrastructure.LintDetectorTest
-import junit.framework.TestCase
 
 class IndirectSuperCallDetectorTest : LintDetectorTest() {
     override fun getDetector() = IndirectSuperCallDetector()
     override fun getIssues() = listOf(IndirectSuperCallDetector.ISSUE)
 
-    fun `test foo`() {
+    fun `test clean`() {
+        val code = """
+            package com.faithlife
+
+            open class Activity {
+                open fun onCreate() {
+                }
+            }
+
+            class SpecialActivity : Activity {
+                override fun onCreate() {
+                    super.onCreate()
+                    println("This method is kinda useless.")
+                }
+
+                private fun setTheme() {
+                    println("Setting the theme.")
+                }
+            }
+        """.trimIndent()
+
+        lint().files(kotlin(code))
+            .run()
+            .expectClean()
+    }
+
+    fun `test adjacent super call`() {
         val code = """
             package com.faithlife
 
